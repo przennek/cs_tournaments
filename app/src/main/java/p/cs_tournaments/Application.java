7c10420cb494afb95a5590d67202c77f5ee1c52c
przennek <przennek@gmail.com>
2016-06-04 09:38:23 +0200

    inital push

---
+++ app/src/main/java/p/cs_tournaments/Application.java
@@ -0,0 +1,13 @@
+package p.cs_tournaments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Application extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_application);
+    }
+}
